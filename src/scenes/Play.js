76924cbde8eaa98979a81548644ab1006d82c72b
Vincent Kaufman <vikaufma@ucsc.edu>
2022-04-13 03:35:40 -0700

    change3

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,8 +2,15 @@ class Play extends Phaser.Scene {
     constructor(){
         super("playScene");
     }
-
+    preload(){
+        // load images/title sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
     create() {
+        // place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
         // white borders
@@ -11,5 +18,10 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+        //add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+    }
+    update(){
+        this.starfield.tilePositionX -= 4;
     }
 }
