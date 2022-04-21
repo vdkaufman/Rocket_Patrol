faaf6f2bd8f766b62d5e510e78c6ab6293abb736
Vincent Kaufman <99847065+vdkaufman@users.noreply.github.com>
2022-04-21 16:35:06 -0700

    Add files via upload

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -3,7 +3,7 @@ class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);   // add to existing scene
-        this.points = 5;   // store pointValue
+        this.points = pointValue;   // store pointValue
         this.moveSpeed = game.settings.spaceshipSpeed;         // pixels per frame
     }
 
@@ -11,15 +11,13 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         // move spaceship left
         this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
-        if(this.x <= 110 - this.width) {
-            this.x = game.config.width - 110;
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width;
         }
-
-
     }
 
     // position reset
     reset() {
-        this.x = game.config.width - 110;
+        this.x = game.config.width;
     }
 }
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,9 +5,9 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load audio
-        this.load.audio('sfx_select', './assets/zapsplat_musical_ice_cream_van_musical_chime_002_43590.mp3');
-        this.load.audio('sfx_explosion', './assets/zapsplat_cartoon_bubble_pop_005_40277.mp3');
-        this.load.audio('sfx_rocket', './assets/zapsplat_cartoon_bubble_002_46660.mp3');
+        this.load.audio('sfx_select', './assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', './assets/explosion38.wav');
+        this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
       }
       
     create() {
@@ -15,8 +15,8 @@ class Menu extends Phaser.Scene {
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#FFBEFA',
-            color: '#313131',
+            backgroundColor: '#F3B141',
+            color: '#843605',
             align: 'right',
             padding: {
                 top: 5,
@@ -25,20 +25,14 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
 
-
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - 
-        borderPadding, ' Ice Cream Patrol ', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#ACD5FD';
-        menuConfig.color = '#313131';
-        this.add.text(game.config.width/2, game.config.height/2, ' Use ←→ arrows to move ', 
-        menuConfig).setOrigin(0.5);
+        borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        menuConfig.backgroundColor = '#00FF00';
+        menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + 
-        borderPadding, ' Use (F) to throw ice cream ', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#FFBEFA';
-        menuConfig.color = '#313131';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + 
-        borderPadding + 45, ' Press ← for Novice or → for Expert ', menuConfig).setOrigin(0.5);
+        borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
     
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
@@ -48,8 +42,8 @@ class Menu extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyLEFT)) {
           // easy mode
           game.settings = {
-            spaceshipSpeed: 3,
-            gameTimer: 60000     
+            spaceshipSpeed: 2,
+            gameTimer: 60000    
           }
           this.sound.play('sfx_select');
           this.scene.start('playScene');    
@@ -57,7 +51,7 @@ class Menu extends Phaser.Scene {
         if (Phaser.Input.Keyboard.JustDown(keyRIGHT)) {
           // hard mode
           game.settings = {
-            spaceshipSpeed: 5,
+            spaceshipSpeed: 2,
             gameTimer: 45000    
           }
           this.sound.play('sfx_select');
